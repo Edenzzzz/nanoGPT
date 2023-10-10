c06ab32ee38b036c3efaf632691b962da663e3f6
Edenzzzz <wenxuan.tan@wisc.edu>
2023-10-10 11:50:57 -0500

    update config

--- config/train_shakespeare_char_new.py
+++ config/train_shakespeare_char_new.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-harrypotter-char'
+out_dir = 'out-shakespeare-char-new'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -10,23 +10,23 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'harrypotter-char'
+wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
-dataset = 'harry_potter'
+dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 350 # context of up to 256 previous characters
+block_size = 300 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 8
-n_head = 6
-n_embd = 384
+n_layer = 7
+n_head = 7
+n_embd = 448
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 7000
-lr_decay_iters = 7000 # make equal to max_iters usually
+max_iters = 6000
+lr_decay_iters = 6000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
